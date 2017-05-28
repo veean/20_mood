3a4958ee00a3961899eafe4f5d8efa0ab94cc592
veean <nersesowm@gmail.com>
2017-05-29 01:46:43 +0400

    add skeleton of project and some static files

---
+++ static/js/randomizer.js
@@ -0,0 +1,15 @@
+<script>
+  var a = document.getElementsById("quotes_json")
+  var b = JSON.parse(a.text)
+  var rand_index = Math.round
+
+
+</script>
+
+$( document.body ).ready(function() {
+    jQuery.getJSON('static/json/quotes.json', success=function(json) {
+        var quote = parseInt(Math.random() * json.quotes.length);
+        $('#joke').prepend(json.quotes[quote].phrase);
+        $('#signature').text(json.quotes[quote].signature);
+    })
+});
